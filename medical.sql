7e4b407c090bf850611baec7f2d5e34280482855
payton <payton@paytontaylor.net>
2022-12-05 16:39:53 -0600

    all files upload

---
+++ medical.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS medical_center_db;
+
+CREATE DATABASE medical_center_db;
+
+\c medical_center_db
+
+CREATE TABLE doctors
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    specialty TEXT NOT NULL
+);
+
+CREATE TABLE patients
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULl,
+    insurance TEXT
+);
+
+CREATE TABLE diseases
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+);
+
+CREATE TABLE diagnoses
+(
+    id SERIAL PRIMARY KEY,
+    visit_id INTEGER NOT NULL,
+    disease_id INTEGER NOT NULL,
+    FOREIGN KEY (visit_id) REFERENCES visits (id),
+    FOREIGN KEY (disease_id) REFERENCES diseases (id),
+    notes TEXT
+);
+
+CREATE TABLE visits
+(
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL,
+    patient_id INTEGER NOT NULL,
+    FOREIGN KEY (doctor_id) REFERENCES doctors (id),
+    FOREIGN KEY (patient_id) REFERENCES patiens (id),
+    date TEXT NOT NULL
+);
